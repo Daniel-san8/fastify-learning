8607dd07efab1241360637fb04927f7e3bdaa1e4
Daniel San <danielgdt88@hotmail.com>
2025-05-08 20:50:11 -0300

    feat: add querys

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,21 @@
 import fastify from 'fastify';
 import { knex } from './database';
+import { randomUUID } from 'node:crypto';
 
 const app = fastify();
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*');
-  return tables;
+  // const transactions = await knex('transactions')
+  //   .insert({
+  //     id: randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*');
+  // return transactions;
+
+  const transactions = knex('transactions').select('*').where('amount', 1000);
+  return transactions;
 });
 
 app
