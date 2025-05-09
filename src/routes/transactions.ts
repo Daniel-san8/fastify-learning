31928e4f1a59211ab12a575d4831f65970711a0e
Daniel San <danielgdt88@hotmail.com>
2025-05-08 22:01:40 -0300

    feat: add reply in post

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify';
 import { knex } from '../database';
 import { randomUUID } from 'node:crypto';
+import { z } from 'zod';
 
 export default async function transactionsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
@@ -8,14 +9,23 @@ export default async function transactionsRoutes(app: FastifyInstance) {
     return transactions;
   });
 
-  app.post('/', async () => {
-    const response = await knex('transactions')
-      .insert({
-        id: randomUUID(),
-        title: 'new transaction',
-        amount: 5984,
-      })
-      .returning('*');
-    return response;
+  app.post('/', async (req, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      typePayment: z.enum(['credit', 'debit']),
+    });
+    const { title, amount, typePayment } = createTransactionBodySchema.parse(
+      req.body
+    );
+
+    const response = await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: typePayment === 'credit' ? amount : amount * -1,
+    });
+    return reply.status(201).send({
+      message: 'Transação executada com sucesso!',
+    });
   });
 }
