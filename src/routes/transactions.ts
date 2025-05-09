71e0b15539663e463e44864eb1c050e8df3b6045
Daniel San <danielgdt88@hotmail.com>
2025-05-08 21:32:07 -0300

    feat: create route post and get transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,17 +3,19 @@ import { knex } from '../database';
 import { randomUUID } from 'node:crypto';
 
 export default async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/hello', async () => {
-    // const transactions = await knex('transactions')
-    //   .insert({
-    //     id: randomUUID(),
-    //     title: 'Transação de teste',
-    //     amount: 1000,
-    //   })
-    //   .returning('*');
-    // return transactions;
-
-    const transactions = knex('transactions').select('*').where('amount', 1000);
+  app.get('/', async () => {
+    const transactions = knex('transactions').select('*').returning('*');
     return transactions;
+  });
+
+  app.post('/', async () => {
+    const response = await knex('transactions')
+      .insert({
+        id: randomUUID(),
+        title: 'new transaction',
+        amount: 5984,
+      })
+      .returning('*');
+    return response;
   });
 }
