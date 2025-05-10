f891187e02f64c932544285687ae84b13a2db02a
Daniel San <danielgdt88@hotmail.com>
2025-05-10 01:19:23 -0300

    feat: create cookie

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -37,13 +37,34 @@ export default async function transactionsRoutes(app: FastifyInstance) {
       req.body
     );
 
+    let session_id = req.cookies.sessionId;
+
+    if (!session_id) {
+      session_id = randomUUID();
+
+      reply.cookie('sessionId', session_id, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      });
+    }
+
     const response = await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: typePayment === 'credit' ? amount : amount * -1,
+      session_id,
     });
     return reply.status(201).send({
       message: 'Transação executada com sucesso!',
+    });
+  });
+
+  app.get('/summary', async (_req, reply) => {
+    const summary = await knex('transactions').sum('amount', { as: 'amount' });
+
+    return reply.status(200).send({
+      summary,
+      message: 'Sumário retornado com sucesso!',
     });
   });
 }
